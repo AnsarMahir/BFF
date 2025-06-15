31fb3c71671b91fe1fee41558a47c24d2fe4bb49
Ansak Mahir <ansarmmm.21@uom.lk>
2025-06-15 21:47:00 +0530

    small changes

--- src/main/java/com/med4all/bff/security/JwtAuthenticationFilter.java
+++ src/main/java/com/med4all/bff/security/JwtAuthenticationFilter.java
@@ -16,7 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-// security/JwtAuthenticationFilter.java
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
